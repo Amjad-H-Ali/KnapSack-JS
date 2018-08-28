b710adbc72686f48b918f9f4dfeeee8648b0957b
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-27 20:54:57 -0500

    Defined variable for max capacity to carry in knapsack and defined 2D array to Store the result of an Item, Capacity pair to avoid repeating

--- js/ks.js
+++ js/ks.js
@@ -6,4 +6,18 @@ const weight = [3, 9, 2, 1, 7, 3];
 
 const value =  [5, 7, 4, 3, 5, 2];
 
+const capacity = 10; // Max capacity to carry in KnapSack.
+
+
+// Inside of a 2D array, Store the result of an Item, Capacity pair to avoid repeating
+
+let results = [];
+
+for (let i = 0; i < capacity; i ++) 
+
+	results.push([]);
+
+console.log(results);
+
+
 
