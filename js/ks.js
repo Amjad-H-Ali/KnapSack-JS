0073a999d0048db10b1860bb08597860a592d556
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-27 22:37:48 -0500

    Fixed bug concerning index -1 of array

--- js/ks.js
+++ js/ks.js
@@ -1,10 +1,8 @@
 console.log('KnapSack!');
 
-// Items Weight and Corresponding Values
-
-const weight = [3, 9, 2, 1, 7, 3];
-
-const value =  [5, 7, 4, 3, 5, 2];
+// Items' Weights and Corresponding Values
+const weight = [3, 9, 2, 1, 7, 3, 11];
+const value =  [5, 7, 4, 3, 5, 2, 30];
 
 const capacity = 10; // Max capacity to carry in KnapSack.
 
@@ -13,15 +11,18 @@ const capacity = 10; // Max capacity to carry in KnapSack.
 // This way, we increase speed of algo to O(n * capacity) from O(n^2)
 let results = [];
 
-for (let i = 0; i < capacity; i ++) 
-
+for (let i = 0, items = weight.length; i < items; i ++) {
 	results.push([]);
+}
+	
+	
 
 				// Current Item index and Capacity
 const knapSack = (item, capacity) => {
 	let result;
 	// If value was found before, return it. No need to find it again.
-	if (results[item][capacity]) return results[item][capacity];
+	// Note: results[-1] is undefined
+	if (item >= 0 && results[item][capacity]) return results[item][capacity];
 
 	// Set result to 0 if item or capacity equal 0.
 	// Item is -1?, meaning we went through all items
@@ -29,25 +30,30 @@ const knapSack = (item, capacity) => {
 	if (item === -1 || capacity === 0) result = 0;
 
 	// When item is too heavy to put in ks, move on to next item.
-	else if (weight[item] > capacity) knapsack(item - 1, capacity);
+	else if (weight[item] > capacity) result = knapSack(item - 1, capacity);
 
 	// Compare the values of moving on to the next item and putting the item in ks.
 	// Set result equal to greatest value
 	else {
 		// Value of next item
-		const moveOn = knapsack(item - 1, capacity);
+		const moveOn = knapSack(item - 1, capacity);
 														// Since we put item in ks, subtract weight of item from cap.
 		// Value of item in bag + value of next item   // To Find Remaining Capacity
-		const putIn = value[item] + knapsack(item - 1, capacity - weight[item]);
+		const putIn = value[item] + knapSack(item - 1, capacity - weight[item]);
 
 		// Compare bothe values and return the greatest.
 		result = Math.max(moveOn, putIn);
 
 	}
 	// Before returning, store result in array to prevent redundancy.
-	results[index][capacity] = result;
+	// If statement because results[-1] is undefined
+	if (item >= 0) results[item][capacity] = result;
+
 	return result;
 }
-
-
+					// Amount of items and weight capacity of KS.
+					// There are 6 items, but 5 is the index number of the last item in the array.
+const amtOfItems = weight.length - 1;					
+console.log(knapSack(amtOfItems, capacity)); // --> 14, because W [3, 2, 1, 3] = 9
+															  //V [5, 4, 3, 2] = 14
 
